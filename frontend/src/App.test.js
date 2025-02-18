9aa40e2f744602fe669d85eb75faaa99120119cf
demetrio-631978 <demetrio631978@gmail.com>
2025-02-18 13:25:58 +0800

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,22 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Skipping the outdated test
-test.skip('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-// Test for Bingo Game title
 test('renders Bingo Game title', () => {
   render(<App />);
   const titleElement = screen.getByText(/Bingo Game/i);
   expect(titleElement).toBeInTheDocument();
 });
 
-// Test for players and bingo numbers
-test('renders players and bingo numbers', () => {
+test('renders Players and Bingo Numbers', () => {
   render(<App />);
   const playersTitle = screen.getByText(/Players:/i);
   const bingoNumbersTitle = screen.getByText(/Bingo Numbers:/i);
