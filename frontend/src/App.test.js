2adf3ffbae98ea7854349259b9bdb93bd11afbc1
demetrio-631978 <demetrio631978@gmail.com>
2025-02-18 13:32:58 +0800

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,16 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Bingo Game title', () => {
+// Skip outdated test
+test.skip('renders learn react link', async () => {
   render(<App />);
-  const titleElement = screen.getByText(/Bingo Game/i);
+  const linkElement = await screen.findByText(/learn react/i); // This is not needed anymore, since the "learn react" text doesn't exist
+  expect(linkElement).toBeInTheDocument();
+});
+
+// Corrected tests
+test('renders Bingo Game title', async () => {
+  render(<App />);
+  const titleElement = await screen.findByText(/Bingo Game/i);  // Looking for Bingo Game title
   expect(titleElement).toBeInTheDocument();
 });
 
-test('renders Players and Bingo Numbers', () => {
+test('renders players and bingo numbers', async () => {
   render(<App />);
-  const playersTitle = screen.getByText(/Players:/i);
-  const bingoNumbersTitle = screen.getByText(/Bingo Numbers:/i);
+  const playersTitle = await screen.findByText(/Players:/i); // Looking for the "Players:" text
+  const bingoNumbersTitle = await screen.findByText(/Bingo Numbers:/i); // Looking for the "Bingo Numbers:" text
   expect(playersTitle).toBeInTheDocument();
   expect(bingoNumbersTitle).toBeInTheDocument();
 });
